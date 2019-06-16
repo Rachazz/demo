00ff126f79c495b30ab37ef33446b7511ad840c2
Rachazz <40149285+Rachazz@users.noreply.github.com>
2019-06-15 23:26:10 -0500

    Sample

---
+++ application.py
@@ -0,0 +1,39 @@
+import mysql.connector
+from mysql.connector import errorcode
+
+# Obtain connection string information from the portal
+config = {
+  'host':'demoquakes.mysql.database.azure.com',
+  'user':'quakes@demoquakes',
+  'password':'Earth_quake',
+  'database':'quakes'
+}
+try:
+   conn = mysql.connector.connect(**config)
+   print("Connection established")
+except mysql.connector.Error as err:
+  if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+    print("Something is wrong with the user name or password")
+  elif err.errno == errorcode.ER_BAD_DB_ERROR:
+    print("Database does not exist")
+  else:
+    print(err)
+else:
+  cursor = conn.cursor()
+
+  # Read data
+  cursor.execute("SELECT * FROM earthquake;")
+
+
+  rows = cursor.fetchall()
+  print("Read",cursor.rowcount,"row(s) of data.")
+
+  # Print all rows
+  for row in rows:
+  	print("Data row = (%s, %s, %s)" %(str(row[0]), str(row[1]), str(row[2])))
+
+  # Cleanup
+  conn.commit()
+  cursor.close()
+  conn.close()
+  print("Done.Done.Done.")
