4b53621488387cf18a1e7ae23bbeed7257e85dc2
Rachazz <40149285+Rachazz@users.noreply.github.com>
2019-06-16 12:42:19 -0500

    Sample local-Assignment3

--- application.py
+++ application.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask,render_template
 import mysql.connector
 from mysql.connector import errorcode
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World! Assignmnet3 getting hang of it-perfect-sql"
+def index():
+    return render_template('assign3.html')
 
 if __name__ == '__main__':
   app.run()
