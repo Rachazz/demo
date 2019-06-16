7bfa6ada2ffd23b458a75e6cdd08a50c07a7872c
Rachazz <40149285+Rachazz@users.noreply.github.com>
2019-06-16 12:36:17 -0500

    Sample local-Assignment3

--- application.py
+++ application.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World! Assignmnet3 getting hang of it-perfect"
+    return "Hello World! Assignmnet3 getting hang of it-perfect-sql"
 
 if __name__ == '__main__':
   app.run()
