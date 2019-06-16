fd10822a37d3e28cebf43b0b8b77100f5179ac8c
Rachazz <40149285+Rachazz@users.noreply.github.com>
2019-06-16 10:15:07 -0500

    Sample local

--- application.py
+++ application.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World! local directory"
+    return "Hello World! local directory final time banthappa banthappa banthuu"
 
 if __name__ == '__main__':
   app.run()
