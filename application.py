0aa42c68e58f70803ace34c0772ada1993c1f3d0
Rachazz <40149285+Rachazz@users.noreply.github.com>
2019-06-18 15:30:54 -0500

    19

--- application.py
+++ application.py
@@ -29,17 +29,22 @@ def displaydata():
     row=[]
     if request.method=="POST":
         num=int(request.form['num'])
-        magni1=request.form['m1']
-        magni2=request.form['m2']
-        time1=request.form['t1']
-        time2=request.form['t2']
+        #magni1=request.form['m1']
+        #magni2=request.form['m2']
+        #time1=request.form['t1']
+        #time2=request.form['t2']
+        lat1=request.form['lat1']
+        long1=request.form['long1']
+        lat2=request.form['lat2']
+        long2=request.form['long2']
         start = time.time()
         for i in range(1,num):
             #query="SELECT * FROM earthquake"
             #query='select count(*) from earthquake where "mag">\''+magni+'\''
             #query='select count(*) from earthquake where "mag" between \''+magni1+'\' and \''+magni2+'\''
             #query='select "depth" from earthquake where "mag"=2'
-            query='select * from earthquake where "mag" between \''+magni1+'\' and \''+magni2+'\' and "time" between \''+time1+'\'and \''+time2+'\''
+            #query='select * from earthquake where "mag" between \''+magni1+'\' and \''+magni2+'\' and "time" between \''+time1+'\'and \''+time2+'\''
+            query='select * from earthquake where "latitude" between \''+lat1+'\' and \''+lat2+'\' and "longitude" between \''+long1+'\'and \''+long2+'\''
             cursor.execute(query)
             row = cursor.fetchall()
         end = time.time()
@@ -59,10 +64,14 @@ def multiplrun():
 
     if request.method=="POST":
         num=int(request.form['num'])
-        magni1=request.form['m1']
-        magni2=request.form['m2']
-        time1=request.form['t1']
-        time2=request.form['t2']
+        #magni1=request.form['m1']
+        #magni2=request.form['m2']
+        lat1=request.form['lat1']
+        long1=request.form['long1']
+        lat2=request.form['lat2']
+        long2=request.form['long2']
+        #time1=request.form['t1']
+        #time2=request.form['t2']
 
         start = time.time()
         for i in range(0,int(num)):
@@ -71,9 +80,13 @@ def multiplrun():
             #query='select count(*) from earthquake where "mag">\''+magni+'\''
             #query='select count(*) from earthquake where "mag" between \''+magni1+'\' and \''+magni2+'\''
             #query='select "depth" from earthquake where "mag"=2'
-            query='select * from earthquake where "mag" between \''+magni1+'\' and \''+magni2+'\' and "time" between \''+time1+'\'and \''+time2+'\''
+            #query='select * from earthquake where "mag" between \''+magni1+'\' and \''+magni2+'\' and "time" between \''+time1+'\'and \''+time2+'\''
+            query='select * from earthquake where "latitude" between \''+lat1+'\' and \''+lat2+'\' and "longitude" between \''+long1+'\'and \''+long2+'\''
             hash = hashlib.sha224(query.encode('utf-8')).hexdigest()
+
             key="redis_cache:"+hash
+            ### 1st method
+
             if (r.get(key)):
                 print("redis cached!")
             else:
@@ -83,8 +96,19 @@ def multiplrun():
                 for j in row:
                     rows.append(str(j))
                 # Put data into cache for 1 hour
-                r.set(key, pickle.dumps(list(rows)) )
-                r.expire(key, 36);
+                r.set(key, pickle.dumps(list(rows)))
+
+
+            ### 2nd method
+            '''
+            result = r.get(key)
+
+            if not result:
+                cursor.execute(query)
+                row = cursor.fetchall()
+                r.set(key, row)
+            '''
+
         end=time.time()
         executiontime = end - start
         return render_template('count.html', t=executiontime)
